e5d25ec19250d5204dfd810262913ac072c4be7e
itai rozen <itai.rozen@mail.huji.ac.il>
2022-04-06 10:49:01 +0300

    server redirects to / if route in addressbar is not found

--- server.js
+++ server.js
@@ -225,7 +225,8 @@ app.get('/stop-cron', (req, res) => {
 })
 
 app.get('/*', (req,res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  // res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.redirect('/')
 })
 
 const tokenManager = () => {
